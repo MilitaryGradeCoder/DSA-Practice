3f36d5219a0bd34ada5578a7cbb50d19604d9c7a
Rahul Singh <rahrocks123@gmail.com>
2026-01-07 22:38:16 +0530

    Time: 2 ms (78.64%) | Memory: 62.5 MB (81.80%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let l = 0;
+    let r = height.length-1;
+    let maxArea = -1;
+
+    while(l<r){
+        let area = (Math.min(height[l], height[r]) * (r-l));
+        maxArea = Math.max(area, maxArea)
+        if(height[l]>=height[r]){
+            r--;
+        }else{
+            l++;
+        }
+    }
+    return maxArea;
+    
+};
