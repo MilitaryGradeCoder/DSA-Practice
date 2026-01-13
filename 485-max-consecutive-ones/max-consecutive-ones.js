5d01b18bbc50d893370909e549a3bdd64982fcf9
Rahul Singh <rahrocks123@gmail.com>
2026-01-13 17:27:13 +0530

    Time: 3 ms (38.56%) | Memory: 60 MB (50.82%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let l = 0;
+    let r = 0;
+    let maxOnes = 0;
+    while(r<nums.length){
+        if(nums[r]===1){
+            r++;
+            maxOnes = Math.max(maxOnes, r-l)
+        }else{
+            r++;
+            l=r;
+        }
+    }
+    return maxOnes;
+};
