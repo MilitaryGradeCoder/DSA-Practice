dfc54cb44483a19d2e3ddb0cd1444ba32dd3bc0b
Rahul Singh <rahrocks123@gmail.com>
2026-01-13 05:46:58 +0530

    Time: 2 ms (82.47%) | Memory: 68.5 MB (42.94%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    if (nums.length == 1) return nums[0];
+    let l = 0;
+    let r = k-1;
+    let sum = 0;
+    for(let i=0; i<k; i++){
+        sum += nums[i];
+    }
+    let maxSum = sum;
+    while(r<nums.length-1){
+        sum = (sum + nums[r+1]) - nums[l];
+        maxSum = Math.max(sum, maxSum);
+        l++;
+        r++;
+    }
+    return maxSum/k;
+};
