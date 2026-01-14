e4dc3d0ae7bc6ec312d2e95eb841e5ecd994db25
Rahul Singh <rahrocks123@gmail.com>
2026-01-15 03:31:25 +0530

    Time: 5 ms (51.26%) | Memory: 60.7 MB (41.88%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.js
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -5,22 +5,16 @@
  */
 var numSubarrayProductLessThanK = function(nums, k) {
    let l=0;
-   let r=0;
    let count=0;
    let product=1;
-   while(r<nums.length){
+   for(let r=0; r<nums.length; r++){
     product *= nums[r];
-    if(product<k){
-        count += (r-l)+1;
-    }
+    if(product<k) count += (r-l)+1;
     while(product>=k && l<r){
         product = product/nums[l];
         l++;
-        if(product<k){
-            count += (r-l)+1;
-        }
+        if(product<k) count += (r-l)+1;
     }
-    r++;
    }
    return count;
 };
