a4d1c7004a3254010d347ba6d10ccf7bc0670991
Rahul Singh <rahrocks123@gmail.com>
2026-01-20 14:59:28 +0530

    Time: 2 ms (86.87%) | Memory: 59.8 MB (41.95%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let l = 0;
+    let r = 0;
+    let sum = 0;
+    let minLen = nums.length+1;
+    while(r<nums.length){
+        if (nums[r]>=target) return 1;
+        sum += nums[r];
+        if(sum>=target){
+            while(l<r && sum>=target){
+                if((r-l+1) < minLen) minLen = r-l+1;
+                sum -= nums[l];
+                l++;
+            }
+        }
+    r++; 
+    }
+    if (minLen<nums.length+1) return minLen;
+    return 0;
+};
