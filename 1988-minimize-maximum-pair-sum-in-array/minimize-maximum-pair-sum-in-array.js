a261d63e460e9161a5c796fd3f2f2e10d8bc0e1c
Rahul Singh <rahrocks123@gmail.com>
2026-01-24 18:18:49 +0530

    Time: 273 ms (48.78%) | Memory: 70.3 MB (78.05%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.js
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.js
@@ -8,7 +8,7 @@ var minPairSum = function(nums) {
     let l = 0;
     let r = nums.length-1;
     while(l<r){
-        res = Math.max(res, (nums[l]+nums[r]));
+        if((nums[l]+nums[r])>res) res = (nums[l]+nums[r])
         l++;
         r--;
     }
