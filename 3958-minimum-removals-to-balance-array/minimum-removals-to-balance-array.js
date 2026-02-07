3ebf08c26c4c8d1f7fee44ae0d3f21a0d30a3297
Rahul Singh <rahrocks123@gmail.com>
2026-02-07 18:38:06 +0530

    Time: 84 ms (60.00%) | Memory: 72 MB (90.00%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minRemoval = function(nums, k) {
+    if (nums.length == 1) return 0;
+    nums.sort((a,b)=>a-b);
+        
+    let l = 0;
+    let r = 0;
+    let maxLen = 0;
+    while(r<nums.length){
+        while(k*nums[l]>=nums[r]){
+            maxLen = Math.max(maxLen, (r-l) + 1)
+            r++;
+        }
+        l++;
+    }
+    return (nums.length - maxLen)
+}
