4c0d1f1f4d53709641f0582710e58dd64bbff62f
Rahul Singh <rahrocks123@gmail.com>
2026-03-07 05:25:08 +0530

    Time: 2 ms (86.72%) | Memory: 56.1 MB (89.47%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-     let nextGreater = new Array(nums2.length);
+     let nextGreater = {};
      let myStack = [];
      for(let i=nums2.length - 1; i>=0; i--){
         if (myStack.length === 0 || nums2[i] > myStack[myStack.length - 1]){
@@ -13,26 +13,21 @@ var nextGreaterElement = function(nums1, nums2) {
             }
             myStack.push(nums2[i]);
             if(myStack.length === 1){
-                nextGreater[i] = -1;
+                nextGreater[nums2[i]] = -1;
             }else{
-                nextGreater[i] = myStack[myStack.length - 2]
+                nextGreater[nums2[i]] = myStack[myStack.length - 2]
             }
             
         }else{
-            nextGreater[i] = myStack[myStack.length - 1];
+            nextGreater[nums2[i]] = myStack[myStack.length - 1];
             myStack.push(nums2[i]);
         }
-     }
-
-     let myHash = new Map();
-     for(let i=0; i<nums2.length; i++){
-        myHash.set(nums2[i], nextGreater[i]);
      }
 
      let ans = [];
 
      for (let i=0; i<nums1.length; i++){
-        let ele = myHash.get(nums1[i]);
+        let ele = nextGreater[nums1[i]];
         ans.push(ele);
      }
 
