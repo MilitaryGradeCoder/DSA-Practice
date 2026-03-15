a21f346ca31a983317ff93f2e3307e1f65511db0
Rahul Singh <rahrocks123@gmail.com>
2026-03-15 14:46:43 +0530

    Time: 3 ms (55.01%) | Memory: 61.8 MB (36.57%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -13,40 +13,30 @@
 
 
 var addTwoNumbers = function(l1, l2) {
-    let stack1 = [];
-    let curr = l1;
-    while(curr != null){
-        stack1.push(curr.val);
-        curr = curr.next;
+    let result = [];
+    let curr1 = l1;
+    let curr2 = l2;
+    let carry = 0;
+    while(curr1 != null || curr2 != null){
+        let digit1 = 0;
+        curr1 != null ? digit1 = curr1.val : digit1 = 0;
+        let digit2 = 0;
+        curr2 != null ? digit2 = curr2.val : digit2 = 0;
+        let sum = digit1 + digit2 + carry;
+        carry = 0;
+        carry = (sum - (sum%10))/10;
+        result.push(sum%10);
+        curr1 != null ? curr1 = curr1.next : curr1 = null;
+        curr2 != null ? curr2 = curr2.next : curr2 = null;
     }
-    let num1Arr = []
-    while (stack1.length != 0){
-        let digit  = stack1.pop();
-        num1Arr.push(digit);
-    }
-    
-    curr = l2;
-    while(curr != null){
-        stack1.push(curr.val);
-        curr = curr.next;
-    }
-    let num2Arr = []
-    while (stack1.length != 0){
-        let digit  = stack1.pop();
-        num2Arr.push(digit);
-    }
-    
-    num1 = BigInt(num1Arr.join(""));
-    num2 = BigInt(num2Arr.join(""));
-    result = (num1 + num2).toString();
+    if (carry != 0) result.push(carry);
     
     let resultNode = new ListNode(0);
-    curr = resultNode;
-    
-    for(let i=result.length-1; i>=0; i--){
-        let newNode = new ListNode(parseInt(result[i]));
-        curr.next = newNode;
-        curr = curr.next;
+    let resultCurr = resultNode;
+    for (let i=0; i<result.length; i++){
+        let node = new ListNode(result[i]);
+        resultCurr.next = node;
+        resultCurr = resultCurr.next;
     }
     return resultNode.next;
 };
