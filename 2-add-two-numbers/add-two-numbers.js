905a40df022e08f763eb8c5bceeec1239b422c7d
Rahul Singh <rahrocks123@gmail.com>
2026-03-15 14:52:45 +0530

    Time: 2 ms (85.44%) | Memory: 61.5 MB (66.65%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -13,10 +13,11 @@
 
 
 var addTwoNumbers = function(l1, l2) {
-    let result = [];
     let curr1 = l1;
     let curr2 = l2;
     let carry = 0;
+    let resultNode = new ListNode(0);
+    let resultCurr = resultNode;
     while(curr1 != null || curr2 != null){
         let digit1 = 0;
         curr1 != null ? digit1 = curr1.val : digit1 = 0;
@@ -25,18 +26,16 @@ var addTwoNumbers = function(l1, l2) {
         let sum = digit1 + digit2 + carry;
         carry = 0;
         carry = (sum - (sum%10))/10;
-        result.push(sum%10);
+        let node = new ListNode(sum%10);
+        resultCurr.next = node;
+        resultCurr = resultCurr.next;
         curr1 != null ? curr1 = curr1.next : curr1 = null;
         curr2 != null ? curr2 = curr2.next : curr2 = null;
     }
-    if (carry != 0) result.push(carry);
-    
-    let resultNode = new ListNode(0);
-    let resultCurr = resultNode;
-    for (let i=0; i<result.length; i++){
-        let node = new ListNode(result[i]);
+    if (carry != 0){
+        let node = new ListNode(carry);
         resultCurr.next = node;
-        resultCurr = resultCurr.next;
     }
+       
     return resultNode.next;
 };
