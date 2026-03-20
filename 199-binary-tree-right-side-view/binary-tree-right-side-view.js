13856499704048d0496fdc58007ec7007989bed4
Rahul Singh <rahrocks123@gmail.com>
2026-03-21 00:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (48.49%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    let ans = [];
+    let depth = 0;
+    preorder(root);
+    return ans;
+
+    function preorder(node){
+        if (node==null) return;
+
+        if(ans.length === depth){
+            ans.push(node.val);
+        }
+
+        depth++;
+        preorder(node.right);
+        
+        preorder(node.left);
+        depth--;
+    }
+
+};
