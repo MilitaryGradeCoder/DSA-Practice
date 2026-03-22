22038a5d69029a8e2e0781c0fe5824ad0151c83d
Rahul Singh <rahrocks123@gmail.com>
2026-03-22 09:20:24 +0530

    Time: 3 ms (36.04%) | Memory: 56.4 MB (85.39%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+    for (let i=0; i<nums.length; i++){
+        if(nums[i]%2!=0){
+            nums[i]=1
+        } else {
+            nums[i]=0;
+        }
+    }
+    return nums.sort((a,b)=>a-b);
+};
