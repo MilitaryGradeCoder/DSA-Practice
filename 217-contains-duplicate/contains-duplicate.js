55a3187aabde2e81b4135b1a0ab3de4b20c5b9b7
Rahul Singh <rahrocks123@gmail.com>
2026-03-27 11:30:50 +0530

    Time: 23 ms (31.19%) | Memory: 73 MB (88.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,13 +3,18 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort();
-    let ans = false;
-    for(let i=1; i<nums.length; i++){
-        if(nums[i]===nums[i-1]){
-            ans = true;
-            break;
-        }
+    // nums.sort();
+    // let ans = false;
+    // for(let i=1; i<nums.length; i++){
+    //     if(nums[i]===nums[i-1]){
+    //         ans = true;
+    //         break;
+    //     }
+    // }
+    // return ans;
+    let mySet = new Set();
+    for(let i=0; i<nums.length; i++){
+        mySet.add(nums[i]);
     }
-    return ans;
+    return (mySet.size != nums.length);
 };
