810630331cec579d3c84ec9c4424e249941e0283
Rahul Singh <rahrocks123@gmail.com>
2026-04-01 00:07:08 +0530

    Time: 42 ms (96.69%) | Memory: 59 MB (68.74%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head) return false;
+    let slow = head;
+    let fast = head;
+    let flag = false;
+    while(fast !== null){
+        slow = slow.next;
+        if(fast.next != null){
+            fast = fast.next.next;
+        }else{
+            break;
+        } 
+        if (slow === fast){
+            flag = true;
+            break;
+        }
+    }
+   return flag;
+};
